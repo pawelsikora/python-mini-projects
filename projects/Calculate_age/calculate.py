aa7769c4173f84e78bb8f70093ea2d9765e2f92c
Pawel Sikora <sikor6@gmail.com>
2025-05-13 08:45:56 +0200

    Fix calculate.py
    Fix syntax error

--- projects/Calculate_age/calculate.py
+++ projects/Calculate_age/calculate.py
@@ -13,7 +13,7 @@ def judge_leap_year(year):
 # returns the number of days in each month
 def month_days(month, leap_year):
     if month in [1, 3, 5, 7, 8, 10, 12]:
-        return 31
+          return 31
     elif month in [4, 6, 9, 11]:
         return 30
     elif month == 2 and leap_year:
